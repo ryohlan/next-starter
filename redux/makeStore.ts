b4e8eeabd926e06727d2a685f4fbf76cc2bbc56b
ryohlan <sunsunryohlan@gmail.com>
2018-07-18 18:30:26 +0900

    add saga middleware

--- redux/makeStore.ts
+++ redux/makeStore.ts
@@ -1,14 +1,18 @@
-import { combineReducers, createStore } from 'redux'
+import { combineReducers, createStore, applyMiddleware, compose } from 'redux'
 import { reducers } from '../reducers'
+import createSagaMiddleware from 'redux-saga'
 
 export const makeStore = initialState => {
-  return createStore(
-    combineReducers(reducers),
-    initialState,
-    (true &&
-      (global as any).window &&
-      (global as any).window.__REDUX_DEVTOOLS_EXTENSION__ &&
-      (global as any).window.__REDUX_DEVTOOLS_EXTENSION__()) ||
-      undefined
+  const sagaMiddleware = createSagaMiddleware()
+  const composeEnhancers =
+    typeof window === 'object' && (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+      ? (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__()
+      : compose
+
+  const enhancer = composeEnhancers(
+    applyMiddleware(sagaMiddleware)
+    // other store enhancers if any
   )
+  const store = createStore(combineReducers(reducers), initialState, enhancer)
+  return store
 }
